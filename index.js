0b3fa49c87e909bae437498490c0a4ae828affd8
Stella Mazzillo <stellagress5@gmail.com>
2023-04-05 18:07:18 -0400

    complete js event listeners lab

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
-function addingEventListener() {
+
+
+
+//This way works 
+
+// const input = document.getElementById('button')
+
+// function addingEventListener() {
+
+    
+//     return input.addEventListener('click', alert('I was clicked'))
+
+// }
+
+
+
+
+
+// Or, Stella's way: 
+
+function addingEventListener () {
+
+    return document.getElementById('button').addEventListener('click', () => alert('I was clicked!'))
+
 }
+
+addingEventListener();
+
+
+
+
+// function addingEventListener() {
+// }
